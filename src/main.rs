b6ed26cd7732dd592c1939b5ea25a7f73237d866
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2021-02-03 14:47:22 +0000

    fix: add warp http listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!")
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let healthz_route = warp::get().map(|| warp::reply::html("{status: 'ok'}"));
+
+    warp::serve(healthz_route).run(([0, 0, 0, 0], 8080)).await;
 }
